22081e0f21bbddf56020acccab8bec8812b5923f
garydev0 <gerardlopezgarcia1@gmail.com>
2026-09-04 00:13:45 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'oppostest-v1';
+const CACHE_NAME = 'oppostest-v2';
 const urlsToCache = [
     './',
     './index.html',
@@ -21,15 +21,37 @@ const urlsToCache = [
 ];
 
 self.addEventListener('install', event => {
+    self.skipWaiting();
     event.waitUntil(
         caches.open(CACHE_NAME)
         .then(cache => cache.addAll(urlsToCache))
     );
 });
 
+self.addEventListener('activate', event => {
+    event.waitUntil(
+        caches.keys().then(cacheNames => {
+            return Promise.all(
+                cacheNames.map(cache => {
+                    if (cache !== CACHE_NAME) return caches.delete(cache);
+                })
+            );
+        })
+    );
+});
+
 self.addEventListener('fetch', event => {
     event.respondWith(
-        caches.match(event.request)
-        .then(response => response || fetch(event.request))
+        fetch(event.request)
+        .then(response => {
+            // Si hay internet, guarda la nueva versión y la muestra
+            const resClone = response.clone();
+            caches.open(CACHE_NAME).then(cache => cache.put(event.request, resClone));
+            return response;
+        })
+        .catch(() => {
+            // Si no hay internet, muestra la versión guardada
+            return caches.match(event.request);
+        })
     );
 });
